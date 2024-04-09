d825050f39a9188b7e4d26d0a5e6e4aa6f925c70
Billo597 <162432016+Billo597@users.noreply.github.com>
2024-04-09 12:06:51 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,8 +1,25 @@
-console.log('Hello!');
-function doLogin()  {
+function doLogin (d) {
   var username = document.getElementById("username").value;
-  var psw  = document.getElementById("password").value;
-  if(username =="" || psw ==""){
-    alert("username e password obbligatori");
+  var psw = document.getElementById("password").value;
+  if(username == "" || psw == "")   {
+      alert("username e password obbligatori");
   }
+  else {
+          fetch('https://www.cacciaapi.altervista.org/login.php/loginIpad', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                  "exampleInputUsername": username,
+                  "exampleInputPassword": password
+              })
+          })
+          .then(response => responsive.json())
+          .then(data => console.log(data))
+          .catch((error) =>{
+              console.error('Error:', error);
+          });
+
+      }
 }
